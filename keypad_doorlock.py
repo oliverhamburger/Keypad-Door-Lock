144d6ab52d1f9d296176752d0868bc16311b4cb6
root <oliver.hamburger@uvm.edu>
2020-04-22 21:34:35 -0400

    Final Changes

--- keypad_doorlock.py
+++ keypad_doorlock.py
@@ -1,8 +1,10 @@
 #servo GPIO pins
 #yellow servo wire to pin 11
 #orange (middle) wire to pin 4
-#brown wire to pin 6 
+#brown wire to pin 6
 
+
+#Code Reference - https://maker.pro/raspberry-pi/tutorial/how-to-use-a-keypad-with-a-raspberry-pi-4 
 
 
 
@@ -15,27 +17,16 @@ class Number:
         self.num = num
 
 
-# This program allows a user to enter a
-# Code. If the C-Button is pressed on the
-# keypad, the input is reset. If the user
-# hits the A-Button, the input is checked.
-
-
-# These are the GPIO pin numbers (for the keypad) where the
-# lines of the keypad matrix are connected
 L1 = 29
 L2 = 31
 L3 = 33
 L4 = 35
 
-# These are the four columns
 C1 = 32
 C2 = 36
 C3 = 38
 C4 = 40
 
-# The GPIO pin of the column of the key that is currently
-# being held down or -1 if no key is pressed
 keypadPressed = -1
 
 secretCode = "123"
@@ -57,14 +48,11 @@ GPIO.setup(L2, GPIO.OUT)
 GPIO.setup(L3, GPIO.OUT)
 GPIO.setup(L4, GPIO.OUT)
 
-# Use the internal pull-down resistors
 GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# This callback registers the key that was pressed
-# if no other key is currently pressed
 
 
 def keypadCallback(channel):
@@ -73,16 +61,11 @@ def keypadCallback(channel):
         keypadPressed = channel
 
 
-# Detect the rising edges on the column lines of the
-# keypad. This way, we can detect if the user presses
-# a button when we send a pulse.
 GPIO.add_event_detect(C1, GPIO.RISING, callback=keypadCallback)
 GPIO.add_event_detect(C2, GPIO.RISING, callback=keypadCallback)
 GPIO.add_event_detect(C3, GPIO.RISING, callback=keypadCallback)
 GPIO.add_event_detect(C4, GPIO.RISING, callback=keypadCallback)
 
-# Sets all lines to a specific state. This is a helper
-# for detecting when the user releases a button
 
 
 def setAllLines(state):
@@ -132,14 +115,10 @@ def checkSpecialKeys():
 
     return pressed
 
-# reads the columns and appends the value, that corresponds
-# to the button, to a variable
 
 
 def readLine(line, characters):
     global input
-    # We have to send a pulse on each line to
-    # detect button presses
     GPIO.output(line, GPIO.HIGH)
     if(GPIO.input(C1) == 1):
         input = input + characters[0]
@@ -154,15 +133,12 @@ def readLine(line, characters):
 
 try:
     while True:
-        # If a button was previously pressed,
-        # check, whether the user has released it yet
         if keypadPressed != -1:
             setAllLines(GPIO.HIGH)
             if GPIO.input(keypadPressed) == 0:
                 keypadPressed = -1
             else:
                 time.sleep(0.1)
-        # Otherwise, just read the input
         else:
             if not checkSpecialKeys():
                 readLine(L1, ["1", "2", "3", "A"])
